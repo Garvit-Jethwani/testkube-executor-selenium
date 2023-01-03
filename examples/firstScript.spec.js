9011bc2e10aafa6a668a9cd566ca131db4cce51d
Garvit Jethwani <44478489+Garvit-Jethwani@users.noreply.github.com>
2023-01-03 19:54:50 +0530

    Update firstScript.spec.js

--- examples/firstScript.spec.js
+++ examples/firstScript.spec.js
@@ -21,7 +21,6 @@ suite(function (env) {
       assert.equal("Web form", title);
 
       await driver.manage().setTimeouts({implicit: 500});
-
       let textBox = await driver.findElement(By.name('my-text'));
       let submitButton = await driver.findElement(By.css('button'));
 
@@ -31,6 +30,7 @@ suite(function (env) {
       let message = await driver.findElement(By.id('message'));
       let value = await message.getText();
       assert.equal("Received!", value);
-    });
+    })
   });
 });
+
